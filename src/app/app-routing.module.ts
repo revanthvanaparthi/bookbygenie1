4cc67b643d183f5c18fe19725c5567574fce3ab4
revanthvanaparthi <33464737+revanthvanaparthi@users.noreply.github.com>
2021-04-28 19:43:45 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { BollywoodComponent } from "./bollywood/bollywood.component";
 import { HollywoodComponent } from "./hollywood/hollywood.component";
 import { SignupComponent } from "./signup/signup.component";
 import { TollywoodComponent } from "./tollywood/tollywood.component";
+import { ScreensComponent } from "./screens/screens.component";
 
 const routes: Routes = [
+  { path: "Screen", component: ScreensComponent },
   { path: "SignUp", component: SignupComponent },
   { path: "Movies,Sports&Events", component: TollywoodComponent },
   { path: "Tickets", component: BollywoodComponent },
@@ -24,5 +26,6 @@ export const routingComponent = [
   HollywoodComponent,
   BollywoodComponent,
   SignupComponent,
+  ScreensComponent,
   AppComponent
 ];
