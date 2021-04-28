cb54722aca8c13b614ff61150d97883bbb6dc0b2
revanthvanaparthi <33464737+revanthvanaparthi@users.noreply.github.com>
2021-04-28 17:54:34 +0530

    Update bollywood.component.ts

--- src/app/bollywood/bollywood.component.ts
+++ src/app/bollywood/bollywood.component.ts
@@ -10,6 +10,14 @@ import { SevicingService } from "../sevicing.service";
 export class BollywoodComponent implements OnInit {
   constructor(private ser: SevicingService) {}
   public M: Movies = this.ser.getBookByGenie();
-
+  public d: Date = new Date();
+  public day: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  public theaters: string[] = [
+    "Shanti Theatre",
+    "INOX GVK One Mall",
+    "PVR Cinemas",
+    "TrendSet"
+  ];
+  public shows: string[] = ["11:00AM", "02:45PM", "07:00PM", "09:30PM"];
   ngOnInit(): void {}
 }
