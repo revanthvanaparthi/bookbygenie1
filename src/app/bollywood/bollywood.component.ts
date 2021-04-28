9d7aa5791329609fbaed25dee8331ca9f5f2106c
revanthvanaparthi <33464737+revanthvanaparthi@users.noreply.github.com>
2021-04-28 16:17:47 +0530

    Update bollywood.component.ts

--- src/app/bollywood/bollywood.component.ts
+++ src/app/bollywood/bollywood.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Movies } from "../movies";
+import { SevicingService } from "../sevicing.service";
 
 @Component({
-  selector: 'app-bollywood',
-  templateUrl: './bollywood.component.html',
-  styleUrls: ['./bollywood.component.less']
+  selector: "app-bollywood",
+  templateUrl: "./bollywood.component.html",
+  styleUrls: ["./bollywood.component.less"]
 })
 export class BollywoodComponent implements OnInit {
+  constructor(private ser: SevicingService) {}
+  public M: Movies = this.ser.getBookByGenie();
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
